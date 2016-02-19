43b9439c18cd5142df9ab5c07c9203042a6e4bc5
Mario Machado <mario.machado94@gmail.com>
2016-02-19 16:28:48 -0500

    Organizing files into client directory

---
+++ client/routes.js
@@ -0,0 +1,65 @@
+FlowRouter.route('/', {
+	  name: "home-page",
+	  action:function(params){
+		  BlazeLayout.render("mainLayout", {content: "homePage"});
+	  }
+});
+
+FlowRouter.route('/signup', {
+	action: function() {
+    	BlazeLayout.render("mainLayout", {content: "signupPage"});
+  	}
+});
+
+FlowRouter.route('/login', {
+	action: function () {
+		BlazeLayout.render("mainLayout", {content: "loginPage"})
+	}
+});
+
+
+FlowRouter.route('/discover', {
+	  name: "discover-page",
+	  action: function(params){
+	  		BlazeLayout.render("mainLayout", {content: "discoverLayout"});
+	  		//$('body').css('background-image','url(body-background.jpg)');
+
+	  }
+  });
+
+FlowRouter.route('/Projects', {
+	  name: "projects-page",
+	  action: function(params){
+	  		BlazeLayout.render("mainLayout", {content: "projectsLayout"});
+	  }
+  });
+
+FlowRouter.route('/Project', {
+	  name: "projects-page",
+	  action: function(params){
+	  		BlazeLayout.render("mainLayout", {content: "projectPage"});
+	  }
+  });
+
+
+routeToProjects = function(){
+  FlowRouter.route('/Projects', {
+	  name: "projects-page",
+	  action: function(params){
+	  		BlazeLayout.render("projectsLayout");
+	  }
+  });
+   FlowRouter.go('/Projects');
+
+}
+
+routeToProjectPage = function(){
+  FlowRouter.route('/Project', {
+	  name: "projects-page",
+	  action: function(params){
+	  		BlazeLayout.render("projectPage");
+	  }
+  });
+   FlowRouter.go('/Project');
+
+}
